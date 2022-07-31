4100029cc90b3c784d2247ea1685aaa47bb319c1
Nico Weidenfeller <nico.weidenfeller@gmail.com>
2022-07-31 15:00:43 +0200

    Code update

--- README.md
+++ README.md
@@ -1,2 +1,245 @@
+
 # Sorting
-SortingAlgorithms
+
+This is a simple WebPage which visualizes the way some sorting algorithms work
+
+# Options
+- Generate: Generates new random data
+- Reset: Resets the displayment of the finished sorting algorithm to the unsored data before
+- Bubble Sort: Executes the bubble sort algorithm
+- Selection Sort: Executes the selection sort algorithm
+- Insertion Sort: Executes the insertion sort algorithm
+- Counting Sort: Executes the counting sort algorithm
+- Radix Sort: Executes the Radix sort algorithm
+- Heap Sort: Executes the heap sort algorithm
+- Shell Sort: Executes the shellsort algorithm
+# UI
+
+# Sorting Algortihms
+Source Code is in "C"
+## Bubble Sort
+```
+void bubbleSort(int array[], int size) {
+
+  // loop to access each array element
+  for (int step = 0; step < size - 1; ++step) {
+      
+    // loop to compare array elements
+    for (int i = 0; i < size - step - 1; ++i) {
+      
+      // compare two adjacent elements
+      // change > to < to sort in descending order
+      if (array[i] > array[i + 1]) {
+        
+        // swapping occurs if elements
+        // are not in the intended order
+        int temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+      }
+    }
+  }
+}
+```
+## Selection Sort
+
+```
+void selectionSort(int array[], int size) {
+  for (int step = 0; step < size - 1; step++) {
+    int min_idx = step;
+    for (int i = step + 1; i < size; i++) {
+
+      // To sort in descending order, change > to < in this line.
+      // Select the minimum element in each loop.
+      if (array[i] < array[min_idx])
+        min_idx = i;
+    }
+
+    // put min at the correct position
+    swap(&array[min_idx], &array[step]);
+  }
+}
+
+// function to swap the the position of two elements
+void swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+```
+## Insertion Sort
+```
+void insertionSort(int array[], int size) {
+  for (int step = 1; step < size; step++) {
+    int key = array[step];
+    int j = step - 1;
+
+    // Compare key with each element on the left of it until an element smaller than
+    // it is found.
+    // For descending order, change key<array[j] to key>array[j].
+    while (key < array[j] && j >= 0) {
+      array[j + 1] = array[j];
+      --j;
+    }
+    array[j + 1] = key;
+  }
+}
+```
+## Counting Sort
+```
+void countingSort(int array[], int size) {
+  int output[10];
+
+  // Find the largest element of the array
+  int max = array[0];
+  for (int i = 1; i < size; i++) {
+    if (array[i] > max)
+      max = array[i];
+  }
+
+  // The size of count must be at least (max+1) but
+  // we cannot declare it as int count(max+1) in C as
+  // it does not support dynamic memory allocation.
+  // So, its size is provided statically.
+  int count[10];
+
+  // Initialize count array with all zeros.
+  for (int i = 0; i <= max; ++i) {
+    count[i] = 0;
+  }
+
+  // Store the count of each element
+  for (int i = 0; i < size; i++) {
+    count[array[i]]++;
+  }
+
+  // Store the cummulative count of each array
+  for (int i = 1; i <= max; i++) {
+    count[i] += count[i - 1];
+  }
+
+  // Find the index of each element of the original array in count array, and
+  // place the elements in output array
+  for (int i = size - 1; i >= 0; i--) {
+    output[count[array[i]] - 1] = array[i];
+    count[array[i]]--;
+  }
+
+  // Copy the sorted elements into original array
+  for (int i = 0; i < size; i++) {
+    array[i] = output[i];
+  }
+}
+```
+## Radix Sort
+```
+// Function to get the largest element from an array
+int getMax(int array[], int n) {
+  int max = array[0];
+  for (int i = 1; i < n; i++)
+    if (array[i] > max)
+      max = array[i];
+  return max;
+}
+
+// Using counting sort to sort the elements in the basis of significant places
+void countingSort(int array[], int size, int place) {
+  int output[size + 1];
+  int max = (array[0] / place) % 10;
+
+  for (int i = 1; i < size; i++) {
+    if (((array[i] / place) % 10) > max)
+      max = array[i];
+  }
+  int count[max + 1];
+
+  for (int i = 0; i < max; ++i)
+    count[i] = 0;
+
+  // Calculate count of elements
+  for (int i = 0; i < size; i++)
+    count[(array[i] / place) % 10]++;
+    
+  // Calculate cumulative count
+  for (int i = 1; i < 10; i++)
+    count[i] += count[i - 1];
+
+  // Place the elements in sorted order
+  for (int i = size - 1; i >= 0; i--) {
+    output[count[(array[i] / place) % 10] - 1] = array[i];
+    count[(array[i] / place) % 10]--;
+  }
+
+  for (int i = 0; i < size; i++)
+    array[i] = output[i];
+}
+
+// Main function to implement radix sort
+void radixsort(int array[], int size) {
+  // Get maximum element
+  int max = getMax(array, size);
+
+  // Apply counting sort to sort elements based on place value.
+  for (int place = 1; max / place > 0; place *= 10)
+    countingSort(array, size, place);
+}
+```
+## Heap Sort
+```
+// Function to swap the the position of two elements
+  void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+  }
+  
+  void heapify(int arr[], int n, int i) {
+    // Find largest among root, left child and right child
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+  
+    if (left < n && arr[left] > arr[largest])
+      largest = left;
+  
+    if (right < n && arr[right] > arr[largest])
+      largest = right;
+  
+    // Swap and continue heapifying if root is not largest
+    if (largest != i) {
+      swap(&arr[i], &arr[largest]);
+      heapify(arr, n, largest);
+    }
+  }
+  
+  // Main function to do heap sort
+  void heapSort(int arr[], int n) {
+    // Build max heap
+    for (int i = n / 2 - 1; i >= 0; i--)
+      heapify(arr, n, i);
+  
+    // Heap sort
+    for (int i = n - 1; i >= 0; i--) {
+      swap(&arr[0], &arr[i]);
+  
+      // Heapify root element to get highest element at root again
+      heapify(arr, i, 0);
+    }
+  }
+```
+## Shell Sort
+```
+void shellSort(int array[], int n) {
+  // Rearrange elements at each n/2, n/4, n/8, ... intervals
+  for (int interval = n / 2; interval > 0; interval /= 2) {
+    for (int i = interval; i < n; i += 1) {
+      int temp = array[i];
+      int j;
+      for (j = i; j >= interval && array[j - interval] > temp; j -= interval) {
+        array[j] = array[j - interval];
+      }
+      array[j] = temp;
+    }
+  }
+}
+```
--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-const arraySize = 400;
-const stepInterval = 0;
+// ================================ Important Config Values ================================ //
+const arraySize = 500;
+const stepInterval = 1;
 const minArrayValue = 0;
-const maxArrayValue = 100;
+const maxArrayValue = 1000000;
+
+// ================================================================ Main Code ================================================================ //
 var sortingArray = [];
+var memArray = [];
 var generatedArray = [];
 var sortingOngoing = false;
 
 window.addEventListener(
   "DOMContentLoaded",
   function () {
-    document.getElementById("scoreboardStepInterval").innerHTML =
-      stepInterval.toString();
     document.getElementById("scoreboardArraySize").innerHTML =
       arraySize.toString();
+    document.getElementById("scoreboardMinArrayValues").innerHTML =
+      minArrayValue.toString();
+    document.getElementById("scoreboardMaxArrayValues").innerHTML =
+      maxArrayValue.toString();
     generateFunction();
   },
   false
@@ -23,7 +29,6 @@ function generateFunction() {
     console.log("Generating new random Data");
     randomizeArray();
     drawArrayToCanvas(generatedArray);
-    document.getElementById("scoreboardTime").innerHTML = "Not set";
   }
 }
 
@@ -31,14 +36,12 @@ function resetFunction() {
   if (!sortingOngoing) {
     console.log("Reset Data");
     drawArrayToCanvas(generatedArray);
-    document.getElementById("scoreboardTime").innerHTML = "Not set";
   }
 }
 
 // ================================ Bubble Sort ================================ //
 function bubbleSortFunction() {
   if (!sortingOngoing) {
-    document.getElementById("scoreboardTime").innerHTML = "Calculating...";
     sortingOngoing = true;
     document.getElementById("scoreboardSortActive").innerHTML = "Bubble Sort";
     for (i = 0; i < generatedArray.length; i++) {
@@ -50,7 +53,6 @@ function bubbleSortFunction() {
 
 async function bubbleSort(arr) {
   var len = arr.length;
-  var startTime = Date.now();
   for (var i = 0; i < len; i++) {
     for (var j = 0; j < len - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
@@ -61,18 +63,15 @@ async function bubbleSort(arr) {
     }
     await drawCanvasTask();
   }
-  var endTime = Date.now();
-  document.getElementById("scoreboardTime").innerHTML =
-    endTime - startTime + "ms";
   sortingOngoing = false;
   document.getElementById("scoreboardSortActive").innerHTML = "None";
+  await drawCanvasTask(); // Clear draw
   return arr;
 }
 
 // ================================ Selection Sort ================================ //
 function selectionSortFunction() {
   if (!sortingOngoing) {
-    document.getElementById("scoreboardTime").innerHTML = "Calculating...";
     sortingOngoing = true;
     document.getElementById("scoreboardSortActive").innerHTML =
       "Selection Sort";
@@ -85,7 +84,6 @@ function selectionSortFunction() {
 
 async function selectionSort(arr) {
   let n = arr.length;
-  var startTime = Date.now();
   for (let i = 0; i < n; i++) {
     let min = i;
     for (let j = i + 1; j < n; j++) {
@@ -100,18 +98,15 @@ async function selectionSort(arr) {
     }
     await drawCanvasTask();
   }
-  var endTime = Date.now();
-  document.getElementById("scoreboardTime").innerHTML =
-    endTime - startTime + "ms";
   sortingOngoing = false;
   document.getElementById("scoreboardSortActive").innerHTML = "None";
+  await drawCanvasTask(); // Clear draw
   return arr;
 }
 
 // ================================ Insertion Sort ================================ //
 function insertionSortFunction() {
   if (!sortingOngoing) {
-    document.getElementById("scoreboardTime").innerHTML = "Calculating...";
     sortingOngoing = true;
     document.getElementById("scoreboardSortActive").innerHTML =
       "Insertion Sort";
@@ -124,7 +119,6 @@ function insertionSortFunction() {
 
 async function insertionSort(arr) {
   let n = arr.length;
-  var startTime = Date.now();
   for (let i = 1; i < n; i++) {
     let current = arr[i];
     let j = i - 1;
@@ -135,39 +129,37 @@ async function insertionSort(arr) {
     arr[j + 1] = current;
     await drawCanvasTask();
   }
-  var endTime = Date.now();
-  document.getElementById("scoreboardTime").innerHTML =
-    endTime - startTime + "ms";
   sortingOngoing = false;
   document.getElementById("scoreboardSortActive").innerHTML = "None";
+  await drawCanvasTask(); // Clear draw
   return arr;
 }
 
 // ================================ Merge Sort ================================ //
+// ToDo
 
 // ================================ Quick Sort ================================ //
+// ToDo
 
 // ================================ Counting Sort ================================ //
 function countingSortFunction() {
   if (!sortingOngoing) {
-    document.getElementById("scoreboardTime").innerHTML = "Calculating...";
     sortingOngoing = true;
     document.getElementById("scoreboardSortActive").innerHTML = "Counting Sort";
     for (i = 0; i < generatedArray.length; i++) {
       sortingArray[i] = generatedArray[i];
     }
-    var startTime = Date.now();
     var min = sortingArray[0];
     var max = sortingArray[0];
     for (i = 0; i < sortingArray.length; i++) {
       min = Math.min(min, sortingArray[i]);
       max = Math.max(max, sortingArray[i]);
     }
-    countingSort(sortingArray, min, max, startTime);
+    countingSort(sortingArray, min, max);
   }
 }
 
-async function countingSort(arr, min, max, startTime) {
+async function countingSort(arr, min, max) {
   const count = {};
   for (let i = min; i <= max; i++) {
     count[i] = 0;
@@ -176,28 +168,220 @@ async function countingSort(arr, min, max, startTime) {
     count[arr[i]] += 1;
   }
   const sortedArr = [];
-  sortingArray = sortedArr;
   for (let i = min; i <= max; i++) {
     while (count[i] > 0) {
       sortedArr.push(i);
       count[i]--;
+      await drawCanvasTask1(sortedArr);
     }
-    await drawCanvasTask();
   }
-
-  var endTime = Date.now();
-  document.getElementById("scoreboardTime").innerHTML =
-    endTime - startTime + "ms";
   sortingOngoing = false;
   document.getElementById("scoreboardSortActive").innerHTML = "None";
+  for (i = 0; i < sortedArr.length; i++) {
+    sortingArray[i] = sortedArr[i];
+  }
+  await drawCanvasTask(); // Clear draw
   return sortedArr;
+}
+
+// ================================ Radix Sort ================================ //
+function radixSortFunction() {
+  if (!sortingOngoing) {
+    sortingOngoing = true;
+    document.getElementById("scoreboardSortActive").innerHTML = "Radix Sort";
+    for (i = 0; i < generatedArray.length; i++) {
+      sortingArray[i] = generatedArray[i];
+    }
+    radixSort(sortingArray);
+  }
+}
+
+async function radixSort(arr) {
+  let maxDigitCount = mostDigits(arr);
+  for (let k = 0; k < maxDigitCount; k++) {
+    let digitBuckets = Array.from({ length: 10 }, () => []);
+    for (let i = 0; i < arr.length; i++) {
+      let digit = getDigit(arr[i], k);
+      digitBuckets[digit].push(arr[i]);
+    }
+    arr = [].concat(...digitBuckets);
+    await drawCanvasTask2(arr, stepInterval * 300);
+  }
+  sortingOngoing = false;
+  document.getElementById("scoreboardSortActive").innerHTML = "None";
+  for (i = 0; i < arr.length; i++) {
+    sortingArray[i] = arr[i];
+  }
+  await drawCanvasTask(); // Clear draw
+  return arr;
+}
+
+function getDigit(num, place) {
+  return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+}
+
+function digitCount(num) {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+function mostDigits(nums) {
+  let maxDigits = 0;
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+  }
+  return maxDigits;
+}
+
+// ================================ Bucket Sort ================================ //
+function bucketSortFunction() {
+  if (!sortingOngoing) {
+    sortingOngoing = true;
+    document.getElementById("scoreboardSortActive").innerHTML = "Bucket Sort";
+    for (i = 0; i < generatedArray.length; i++) {
+      sortingArray[i] = generatedArray[i];
+    }
+    bucketSort(sortingArray, 5);
+  }
+}
+
+async function bucketSort(arr, bucketSize) {
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+  const buckets = Array.from(
+    { length: Math.floor((max - min) / bucketSize) + 1 },
+    () => []
+  );
+  arr.forEach((val) => {
+    buckets[Math.floor((val - min) / bucketSize)].push(val);
+  });
+  x = buckets.reduce((acc, b) => [...acc, ...b.sort((a, b) => a - b)], []);
+  await drawCanvasTask1(x);
+  sortingOngoing = false;
+  document.getElementById("scoreboardSortActive").innerHTML = "None";
+  await drawCanvasTask(); // Clear draw
+  return arr;
+}
+
+// ================================ Heap Sort ================================ //
+function heapSortFunction() {
+  if (!sortingOngoing) {
+    sortingOngoing = true;
+    document.getElementById("scoreboardSortActive").innerHTML = "Heap Sort";
+    for (i = 0; i < generatedArray.length; i++) {
+      sortingArray[i] = generatedArray[i];
+    }
+    heapSort(sortingArray);
+  }
+}
+
+async function heapSort(arr) {
+  buildMaxHeap(arr);
+  lastElement = arr.length - 1;
+  while (lastElement > 0) {
+    swap(arr, 0, lastElement);
+    heapify(arr, 0, lastElement);
+    lastElement -= 1;
+    await drawCanvasTask();
+  }
+  sortingOngoing = false;
+  document.getElementById("scoreboardSortActive").innerHTML = "None";
+  await drawCanvasTask(); // Clear draw
+  return arr;
+}
+
+function buildMaxHeap(arr) {
+  let i = Math.floor(arr.length / 2 - 1);
+  while (i >= 0) {
+    heapify(arr, i, arr.length);
+    i -= 1;
+  }
+}
+
+function heapify(heap, i, max) {
+  let index;
+  let leftChild;
+  let rightChild;
+
+  while (i < max) {
+    index = i;
+    leftChild = 2 * i + 1;
+    rightChild = leftChild + 1;
+    if (leftChild < max && heap[leftChild] > heap[index]) {
+      index = leftChild;
+    }
+    if (rightChild < max && heap[rightChild] > heap[index]) {
+      index = rightChild;
+    }
+    if (index === i) {
+      return;
+    }
+    swap(heap, i, index);
+    i = index;
+  }
+}
+
+function swap(arr, firstItemIndex, lastItemIndex) {
+  const temp = arr[firstItemIndex];
+  arr[firstItemIndex] = arr[lastItemIndex];
+  arr[lastItemIndex] = temp;
+}
+
+// ================================ Shell Sort ================================ //
+function shellSortFunction() {
+  if (!sortingOngoing) {
+    sortingOngoing = true;
+    document.getElementById("scoreboardSortActive").innerHTML = "Shell Sort";
+    for (i = 0; i < generatedArray.length; i++) {
+      sortingArray[i] = generatedArray[i];
+    }
+    shellSort(sortingArray);
+  }
+}
+
+async function shellSort(arr) {
+  let n = arr.length;
+
+  for (let gap = Math.floor(n / 2); gap > 0; gap = Math.floor(gap / 2)) {
+    for (let i = gap; i < n; i += 1) {
+      let temp = arr[i];
+      let j;
+      for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+        arr[j] = arr[j - gap];
+      }
+      arr[j] = temp;
+      await drawCanvasTask();
+    }
+  }
+  sortingOngoing = false;
+  document.getElementById("scoreboardSortActive").innerHTML = "None";
+  await drawCanvasTask(); // Clear draw
+  return arr;
 }
 
 // ================================ Other ================================ //
 function drawCanvasTask() {
   return new Promise(function (resolve) {
     setTimeout(function () {
-      drawArrayToCanvas(sortingArray);
+      drawArrayToCanvas(sortingArray, true);
+      resolve();
+    }, stepInterval);
+  });
+}
+
+function drawCanvasTask1(arr) {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      drawArrayToCanvas(arr, true);
+      resolve();
+    }, stepInterval);
+  });
+}
+
+function drawCanvasTask2(arr, stepInterval) {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      drawArrayToCanvas(arr, true);
       resolve();
     }, stepInterval);
   });
@@ -222,7 +406,7 @@ function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function drawArrayToCanvas(array) {
+function drawArrayToCanvas(array, markChanges = false) {
   var canvas = document.getElementById("canvas");
   const width = (canvas.width = window.innerWidth);
   const height = (canvas.height = window.innerHeight);
@@ -233,9 +417,16 @@ function drawArrayToCanvas(array) {
   var barWidth = width / arraySize;
   var barHeight = height;
 
-  ctx.fillStyle = "#ff6d00";
-
   for (let i = 0; i < arraySize; i++) {
+    if (markChanges) {
+      if (memArray[i] != array[i]) {
+        ctx.fillStyle = "#d50000";
+      } else {
+        ctx.fillStyle = "#00e676";
+      }
+    } else {
+      ctx.fillStyle = "#00e676";
+    }
     var currentBarHeight = barHeight * (array[i] / maxArrayValue);
     ctx.fillRect(
       i * barWidth,
@@ -243,5 +434,9 @@ function drawArrayToCanvas(array) {
       barWidth - barSpacing,
       barHeight * (array[i] / maxArrayValue)
     );
+  }
+  memArray = [];
+  for (i = 0; i < array.length; i++) {
+    memArray[i] = array[i];
   }
 }
